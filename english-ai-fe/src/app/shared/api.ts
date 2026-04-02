bb0b81d7dc9bb62e46368499daf9921bada8a81f
Mayur Talekar <165009347+mayur-talekar-tc@users.noreply.github.com>
2026-04-02 23:30:02 +0530

    be deployed

--- english-ai-fe/src/app/shared/api.ts
+++ english-ai-fe/src/app/shared/api.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://127.0.0.1:3001/api';
+export const API_BASE_URL = 'https://bhashaai-backend.onrender.com/api';
 export const AI_API_URL = `${API_BASE_URL}/ai`;
 export const AUTH_API_URL = `${API_BASE_URL}/auth`;
