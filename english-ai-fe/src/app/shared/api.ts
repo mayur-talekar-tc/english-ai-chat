8c911b749cf4e2591d5c9264b3125966f8f5be0c
Mayur Talekar <165009347+mayur-talekar-tc@users.noreply.github.com>
2026-05-26 20:45:33 +0530

    basic hanegs

--- english-ai-fe/src/app/shared/api.ts
+++ english-ai-fe/src/app/shared/api.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://english-ai-chat-cws7.onrender.com';
-export const API_BASE_URL = 'http://localhost:3001/api';
+export const API_BASE_URL = 'https://english-ai-chat-cws7.onrender.com';
+// export const API_BASE_URL = 'http://localhost:3001/api';
 export const AI_API_URL = `${API_BASE_URL}/ai`;
 export const AUTH_API_URL = `${API_BASE_URL}/auth`;
 export const PROGRESS_API_URL = `${API_BASE_URL}/progress`;
